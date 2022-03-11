98beef2140988da6018fc6e30d1cb2db9edd284c
GunalanD95 <guna19may2015@gmail.com>
2022-03-11 06:48:34 +0530

    learn:big-o

---
+++ alogorithms/big-o/big_o1.py
@@ -0,0 +1,9 @@
+# O(N) -   input increases but operations remains constant
+
+
+def LogFirst2(list):
+    print(list[0])
+    print(list[1])
+
+
+LogFirst2(['Guna', 'Krishna', 'Raju', 'Sai'])
---
+++ alogorithms/big-o/bigo_rules.py
@@ -0,0 +1,5 @@
+# There are 4 rules for big-o notation:
+# Rule 1 : Worst Case
+# Rule 2 : Remove Constants
+# Rule 3 : Different terms for inputs
+# Rule 4 : Drop Non Dominants
