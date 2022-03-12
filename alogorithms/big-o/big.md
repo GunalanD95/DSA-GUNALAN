bc66d1c82caa34e22ebc2ba693a9cb7e4b56555d
GunalanD95 <guna19may2015@gmail.com>
2022-03-12 19:51:34 +0530

    comp

--- alogorithms/big-o/big.md
+++ alogorithms/big-o/big.md
@@ -3,3 +3,7 @@
 https://udemy.com/course/master-the-coding-interview-data-structures-algorithms/learn/lecture/12133490#overview
 
 https://www.geeksforgeeks.org/big-o-notation/
+
+https://www.bigocheatsheet.com/
+
+https://www.khanacademy.org/computing/computer-science/algorithms#intro-to-algorithms
---
+++ alogorithms/big-o/space_comp.py
@@ -0,0 +1,20 @@
+# Space Complexity
+
+'''
+    Space complexity is a parallel concept to time complexity. 
+    If we need to create an array of size n,
+    this will require O(n) space. If we create a two-dimensional array of size n*n, 
+    this will require O(n2) space.
+'''
+
+def fooo(n):
+    for i in range(n):
+        print("I WILL PRINT")
+
+
+fooo(10)
+
+
+# Time Complexity - O(n)
+
+# Space Complexity - O(1)
