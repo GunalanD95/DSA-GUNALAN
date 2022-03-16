4426e8f68da095a73aedd2402d86c269f672de3a
GunalanD95 <guna19may2015@gmail.com>
2022-03-16 07:40:01 +0530

    fix:issue

--- data-structures/user-defined/array/imp_arr.py
+++ data-structures/user-defined/array/imp_arr.py
@@ -21,16 +21,23 @@ class Array(object):
         return self.data
 
     def delete(self,num):
-        del self.data[num]
+        item = self.data[num]
+        self.indexitems(num)
+        return item
+
+    def indexitems(self,num):
+        for i in range(num,self.length-1):
+            self.data[i] = self.data[i+1]
+        del self.data[self.length-1]
         self.length -= 1
         return self.data
 
 
 newarr = Array();
-newarr.append(1)
-newarr.append(2)
-newarr.append(3)
-newarr.pop()
+newarr.append(1) # 0
+newarr.append(2) # 1
+newarr.append(3) # 2
+newarr.append(4) # 3
+print("newarr",newarr.data)
 newarr.delete(0)
-newarr.append(4)
 print("newarr",newarr.data)
