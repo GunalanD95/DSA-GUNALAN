13b3e7bdc1bed750c56cb133da7d47725ae57ce5
GunalanD95 <guna19may2015@gmail.com>
2022-03-22 07:29:54 +0530

    add

--- data-structures/user-defined/hash-tables/imp_hash_tab.py
+++ data-structures/user-defined/hash-tables/imp_hash_tab.py
@@ -42,6 +42,22 @@ class HashTable():
         return None
 
 
+    def delete(self, key):
+        hash_key = self._hash_(key)
+        key_exists = False
+        slot = self.hashmap[hash_key]
+        for i , kv in enumerate(slot):
+            k, v = kv 
+            if key == k:
+                key_exists = True 
+                break
+        if key_exists:
+            del slot[i]
+            print ('Key {} deleted'.format(key))
+        else:
+            print ('Key {} not found'.format(key))
+
+
     def __setitem__(self,key,value):
         self.set(key,value)
 
@@ -53,6 +69,8 @@ a.set("key-1","value-1")
 a.set("key-2","value-2")
 a.set("key-3","value-3")
 a.set("key-4","value-4")
-
+a.set("key-34","value-34")
 print(a.hashmap)
+a.delete("key-34")
 print(a.get("key-1"))
+print(a.hashmap)
