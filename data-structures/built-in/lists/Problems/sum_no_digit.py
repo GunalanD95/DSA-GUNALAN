46d98c87765a597e18f0259e9ab7474e7497987d
GunalanD95 <guna19may2015@gmail.com>
2022-03-24 07:05:52 +0530

    linkedls:start

---
+++ data-structures/built-in/lists/Problems/sum_no_digit.py
@@ -0,0 +1,10 @@
+# Python | Sum of number digits in List
+
+test_list = [12, 67, 98, 34]
+
+def find_sum_digits(ls):
+    for i in ls:
+        print(i)
+
+
+find_sum_digits(test_list)
--- data-structures/user-defined/hash-tables/ex_first_recu.py
+++ data-structures/user-defined/hash-tables/ex_first_recu.py
@@ -18,13 +18,34 @@
 a = [2,5,1,2,3,5,1,2,4]
 b = [2,1,1,2,3,5,1,2,4]
 
+c =[2,5,5,2,3,5,1,2,4]
+
 def firstrecurchar(inp):
-    res = []
+    res = {}
     for i in inp:
-        if i not in res:
-            res.append(i)
-        else:
+        if i in res:
             print(f'{i} is the first recurring element')
             break
+        else:
+            res[i] = 0
+            
 firstrecurchar(a)
 firstrecurchar(b)    
+firstrecurchar(c)    
+
+# the big O is O(n)
+
+
+
+# 2 Naive Solution
+
+def FirstRecurringCharacter(input):
+    for i in range(len(input)):
+        for j in range(i+1, len(input)):
+            if input[i] == input[j]:
+                return input[i]
+    return None
+
+
+print('Naive:',FirstRecurringCharacter(a))
+# the big O is O(n^2)
