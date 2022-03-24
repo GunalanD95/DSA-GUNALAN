efe134dfb857f2803b4a5d58d69e8dd6eac99cfc
GunalanD95 <guna19may2015@gmail.com>
2022-03-24 20:39:59 +0530

    prob:arr

--- data-structures/built-in/lists/Problems/sum_no_digit.py
+++ data-structures/built-in/lists/Problems/sum_no_digit.py
@@ -2,9 +2,36 @@
 
 test_list = [12, 67, 98, 34]
 
+
+#  Using Naive Method 
+
 def find_sum_digits(ls):
+    final = []
     for i in ls:
-        print(i)
+        k = [int(d) for d in str(i)]
+        print(i,':',k)
+        final.append(sum(k))
+    return final
 
 
-find_sum_digits(test_list)
+print(find_sum_digits(test_list))
+
+
+# 2 Using loop + str() 
+res = []
+for e in test_list:
+    sum = 0
+    for dj in str(e):
+        sum += int(dj)
+
+    res.append(sum)
+
+print ("List Integer Summation : " + str(res))
+
+
+# 3 Using lambda and list comp
+res3 = list(map(lambda ele: sum(int(sub) for sub in str(ele)), test_list))
+     
+# printing result
+print ("List Integer Summation : " + str(res3))
+
