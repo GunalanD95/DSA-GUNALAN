0c1989c46bc67b8457243258edbf76274477cf36
GunalanD95 <guna19may2015@gmail.com>
2022-03-30 07:58:41 +0530

    fix:ddlk

--- data-structures/user-defined/linked-list/doub_lk.py
+++ data-structures/user-defined/linked-list/doub_lk.py
@@ -7,43 +7,52 @@ class Node:
         self.prev = None
 
     def __str__(self):
-        return str({'value': self.data, 'next': self.next , 'prev': self.prev})
+        return str({'value': self.data, 'next': self.next, 'prev': self.prev})
 
 class DoublyLinkedList:
 
-    def __init__(self,data) -> None:
-        self.head = Node(data)
-        self.tail = self.head
-        self.length = 1
+    def __init__(self) -> None:
+        self.head = None
+        self.tail = None
+        self.length = 0
 
-    def append(self,data):
-        newNode = Node(data)
-        temp = self.tail
-        self.tail = newNode
-        self.tail.prev = temp
-        self.head.next = newNode
-        self.length += 1
+    def append(self,value):
+        newNode = Node(value)
+        if self.head is None:
+            self.head = newNode
+            self.tail = newNode
+            self.length += 1
 
+        else:
+            self.tail.next = newNode
+            newNode.prev = self.tail
+            self.tail = newNode
+            self.length += 1
 
-    def prepend(self,data):
-        newNode = Node(data)
+    def print_list(self):
+        arr = []
         temp = self.head
+        print(temp)
+        while temp is not None:
+            arr.append(temp.data)
+            temp = temp.next
+        return arr
+         
+
+    def prepend(self,value):
+        newNode = Node(value)
+        temp = self.head
+        newNode.next = temp
         self.head = newNode
-        self.head.next = temp
         self.length += 1
 
-    def printlist(self):
-        cur = self.head
-        while cur:
-            print(cur.data)
-            cur = cur.next
 
-    def getLength(self):
-        return print(self.length)
-
-lk = DoublyLinkedList(4)
-lk.append(5)
-lk.prepend(10)
-print(lk.tail)
-print(lk.head) 
-lk.printlist()
+dl = DoublyLinkedList()
+dl.append(1)
+dl.append(2)
+dl.append(3)
+dl.prepend(99)
+print("head",dl.head)
+print("tail",dl.tail)
+print("print_list",dl.print_list())
+                        
