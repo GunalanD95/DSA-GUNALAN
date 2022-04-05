9616c9932e7c08766dc71d7bdf7ed0b4aefe8e96
GunalanD95 <guna19may2015@gmail.com>
2022-04-05 19:50:19 +0530

    prob:list

--- data-structures/built-in/lists/Problems/find_large.py
+++ data-structures/built-in/lists/Problems/find_large.py
@@ -1,37 +1,48 @@
 # Python program to find largest number in a list
 
-
-b = [100,20, 10, 20, 1]
 a = [10, 20,994]
+b = [100,20, 10, 20, 1]
+
 
 
 # 1 Naive approach
 
 def finflarg(ls):
     big = 0
-    for i in ls:
-        if i > big:
+    temp = 0
+    for i in range(0,len(ls)):
+        if ls[i] > temp:
+            temp  = ls[i]
             big = i
 
     return big
 
 
-print(finflarg(a))
-print(finflarg(b))
+# print(finflarg(a))
+# print(finflarg(b))
 
 
 # 2nd Largest element
 
-def findsec(ls):
-    big = ls[0]
-    dev = []
-    for i in ls:
-        if i > big:
-            dev.append(i)
 
-    return dev[-1]
+a = [10, 20,994,555]
+b = [100,20, 10, 20, 1]
+c = [10, 20,44,222,657,653,12,3,555]
+
+def findsec(ls):
+    largest = finflarg(ls)
+    res = -1
+    for i in range(0,len(ls)):
+       if ls[i] != ls[largest]:
+           if res == -1:
+               res = i
+           elif ls[i] > ls[res]:
+               res = i
+
+    return ls[res]
 
 print(findsec(a))
 print(findsec(b))
+print(findsec(c))
 
 
