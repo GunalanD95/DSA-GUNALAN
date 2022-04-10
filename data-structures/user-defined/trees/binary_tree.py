793cc5bfc98b95b04f45de7f570994dd3416c644
GunalanD95 <guna19may2015@gmail.com>
2022-04-10 19:34:26 +0530

    binary:tree-update

--- data-structures/user-defined/trees/binary_tree.py
+++ data-structures/user-defined/trees/binary_tree.py
@@ -7,3 +7,50 @@ class BinaryTreeNode:
         self.value = value
         self.left = None
         self.right = None
+
+
+class BinarySearchTreeNode():
+
+    def __init__(self) -> None:
+        self.root = None
+
+
+    def insert(self,value):
+        newNode = BinaryTreeNode(value)
+        if self.root == None:
+            self.root = newNode
+
+        else:
+            while True:
+                cur =  self.root
+                if value < cur.value:
+                    if cur.left != None:
+                        cur.left = newNode
+                    cur = cur.left
+                    if cur == None:
+                        break
+                    
+                else:
+                    if cur.right != None:
+                        cur.right = newNode
+                    cur = cur.right
+                
+                    
+
+
+    def lookup(self,value):
+        pass
+
+    def __str__(self):
+        return str(self.root)
+
+
+bt = BinarySearchTreeNode()
+bt.insert(9)
+bt.insert(4)
+bt.insert(6)
+bt.insert(20)
+bt.insert(170)
+bt.insert(15)
+bt.insert(1)
+print("bt",bt.root.value)
