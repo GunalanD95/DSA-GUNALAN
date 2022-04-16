9d89fb6581c4095edb71ff0ec3a5ffa226089574
GunalanD95 <guna19may2015@gmail.com>
2022-04-16 10:17:28 +0530

    BST:PreOrderTravs

--- data-structures/user-defined/trees/bst.py
+++ data-structures/user-defined/trees/bst.py
@@ -14,9 +14,12 @@ class BST:
     def __init__(self) -> None:
         self.root = None
 
+    def isEmpty(self):
+        if self.root == None:
+            return True
     
     def insert(self,value):
-        if self.root == None:
+        if self.isEmpty():
             newNode = Node(value)
             self.root = newNode
         else:
@@ -44,11 +47,70 @@ class BST:
         
 
 
-bs = BST()
-bs.insert(10)
-print(bs.insert(11))
-bs.insert(5)
 
-print("root",bs.root.value)
-print("left",bs.root.left.value)
-print("right",bs.root.right.value)
+    def search(self,value):
+        if self.isEmpty():
+            return None
+
+        else:
+            self._search(value,self.root)
+
+    def _search(self,value,curNode):
+        if value == curNode.value:
+            print("found it",curNode.value)
+
+        if value < curNode.value and curNode.left:
+            self._search(value,curNode.left)
+
+        if value > curNode.value:
+            if curNode.right:
+                self._search(value,curNode.right)
+
+
+
+    def preorderTraversal(self):
+        if self.isEmpty():
+            return str("No Data is there")
+
+        else:
+            self._preorderTraversal(self.root)
+
+    
+    def _preorderTraversal(self,curNode):
+        print("data",curNode.value)
+        if curNode.left:
+            self._preorderTraversal(curNode.left)
+        
+        if curNode.right:
+            self._preorderTraversal(curNode.right)
+
+    def inorderTraversal():
+        pass
+
+    def postorderTraversal():
+        pass
+
+
+
+
+bs = BST()
+
+# Insertion
+bs.insert(50)
+bs.insert(25)
+bs.insert(75)
+bs.insert(10)
+bs.insert(35)
+bs.insert(5)
+bs.insert(13)
+bs.insert(30)
+bs.insert(42)
+
+# Lookup
+bs.search(77)
+bs.search(5)
+
+
+# Traversal 
+
+print(bs.preorderTraversal())
