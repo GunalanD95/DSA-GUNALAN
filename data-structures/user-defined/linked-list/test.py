43de23420e20514cd22ef55d4ab35f41de1bd120
GunalanD95 <guna19may2015@gmail.com>
2022-04-25 07:54:41 +0530

    llk:rev

--- data-structures/user-defined/linked-list/test.py
+++ data-structures/user-defined/linked-list/test.py
@@ -11,16 +11,63 @@ class LinkedList():
         self.length = 0
 
     def isEmpty(self):
-        if self.head:
+        if self.head == None:
             return True
 
-    def prepend(self,value):
+    def length(self):
+        return self.length
+
+    def append(self,value):
+        newNode = Node(value)
+
         if self.isEmpty():
-            return None
-        else:
-            newNode = Node(value)
-            temp = self.head
             self.head = newNode
-            self.head.next = temp
-            self.length +=1
+            return
+
+        cur = self.head
+        while cur.next !=None:
+            cur = cur.next
+        cur.next = newNode
+        self.length +=1
+
+    def prepend(self,value):
+        newNode = Node(value)
+        temp = self.head
+        newNode.next = temp
+        self.head = newNode
+        self.length +=1
+
+        
+
+
+
+    def print_list(self):
+        array = []
+        currentNode = self.head
+        while  currentNode != None:
+            array.append(currentNode.value)
+            currentNode = currentNode.next
+        return array
+
+lk = LinkedList()
+
+# Prepend and Append
+
+
+
+print(" ")
+lk.append(20)
+lk.append(25)
+lk.append(30)
+print(" ")
+lk.prepend(5)
+lk.prepend(10)
+lk.prepend(15)
+
+
+
+print("head",lk.head.next)
+
+print(lk.print_list())
+
             
